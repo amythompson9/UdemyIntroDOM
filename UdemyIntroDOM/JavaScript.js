f6e040363c7036d8abd6c2c1107c17fb66165de6
Amy Thompson <amythompson9@gmail.com>
2018-07-05 20:56:17 -0400

    Methods

--- UdemyIntroDOM/JavaScript.js
+++ UdemyIntroDOM/JavaScript.js
@@ -1,1 +1,14 @@
-﻿
+﻿//document.getElementById()
+
+var tag = document.getElementById("highlighted");
+
+
+//document.getElementsByClassName()
+
+var tags = document.getElementByClassName("bolded");
+console.log(tags[0]);
+
+//document.getElementsByTagName()
+
+var tags = document.getElementsByTagName("li");
+console.log(tags[0]);
